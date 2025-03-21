7cf65d677a0b5ea047f6b53f48849ec18d4fc5ac
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-22 01:34:26 +0300

    updated

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Loan(models.Model):
+    borrower = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.borrower} - ${self.amount}"
