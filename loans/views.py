4cecac9866b8d9c417043750f05b6248751df821
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-22 03:02:12 +0300

    updated

--- loans/views.py
+++ loans/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Loan
 
 def index(request):
+    if request.method == 'POST':
+        borrower = request.POST.get('borrower')
+        amount = request.POST.get('amount')
+        if borrower and amount:
+            Loan.objects.create(borrower=borrower, amount=amount)
+        return redirect('index')
     loans = Loan.objects.all()
     return render(request, 'loans/index.html', {'loans': loans})
