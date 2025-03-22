ea2cc1dada23551f73839518431936c5c62a5ca3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-22 04:11:40 +0300

    updated

--- loans/models.py
+++ loans/models.py
@@ -5,5 +5,6 @@ class Loan(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[('Active', 'Active'), ('Paid', 'Paid')], default='Active')
+
     def __str__(self):
-        return f"{self.borrower} - ${self.amount}"
+        return f"{self.borrower} - ${self.amount} ({self.status})"
