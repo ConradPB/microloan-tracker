c7c9e77cb4f42d2b3a10f403b71c87682200e355
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-26 23:27:33 +0300

    updated

--- microloan_tracker/urls.py
+++ microloan_tracker/urls.py
@@ -14,11 +14,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('', include('loans.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('admin/', admin.site.urls),
-]
