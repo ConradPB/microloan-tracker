b4806ad925745f0fb7c44701aa0e025d98bfb15e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-26 06:16:32 +0300

    updated/Tied loans to users

--- loans/models.py
+++ loans/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
     borrower = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
