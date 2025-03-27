e405db302ca223bab21ead2d97e4e059c042c856
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-27 22:25:48 +0300

    updated with login

--- microloan_tracker/urls.py
+++ microloan_tracker/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('loans.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
