3660405356af44c786530752214bdf6645935e02
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-27 14:18:29 +0300

    Added authentication and authorization

--- loans/models.py
+++ loans/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Loan(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Nullable for existing data
     borrower = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     due_date = models.DateField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=[('Active', 'Active'), ('Paid', 'Paid')], default='Active')
+    status = models.CharField(max_length=10, choices=[('Active', 'Active'), ('Paid', 'Paid')], default='Active')
     category = models.CharField(max_length=20, choices=[('Personal', 'Personal'), ('Business', 'Business')], default='Personal')
 
     def __str__(self):
