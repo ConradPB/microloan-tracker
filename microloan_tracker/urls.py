e4b9b3dff4f2108f83853d3ad742db9e4c5b2c89
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-28 22:09:44 +0300

    updated

--- microloan_tracker/urls.py
+++ microloan_tracker/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('loans.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
