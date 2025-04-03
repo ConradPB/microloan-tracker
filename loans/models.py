ac3f809a5dbdff96f075b9f6910e3228b72bc8a2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-04-03 07:10:26 +0300

    updated

--- loans/models.py
+++ loans/models.py
@@ -9,6 +9,7 @@ class Loan(models.Model):
     due_date = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=10, choices=[('Active', 'Active'), ('Paid', 'Paid')], default='Active')
     category = models.CharField(max_length=20, choices=[('Personal', 'Personal'), ('Business', 'Business')], default='Personal')
+    
 
     def __str__(self):
         return f"{self.borrower} - ${self.amount} ({self.status})"
